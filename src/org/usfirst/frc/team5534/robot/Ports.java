f8018cd599b06d29d1cb9fb325b0374cdedcd98c
Scott Steensma <ssteensma@oacsd.com>
2018-01-13 08:33:53 -0500

    Added some notes on the ports file.
    Too many DIO ports needed; listed alternatives.

--- src/org/usfirst/frc/team5534/robot/Ports.java
+++ src/org/usfirst/frc/team5534/robot/Ports.java
@@ -2,6 +2,21 @@ package org.usfirst.frc.team5534.robot;
 
 public class Ports {
 
+	// TODO: Too many DIO ports are allocated so some sensors
+	// will need to be eliminated. Candidates are:
+	// -  DriveEncoderL (2 ports)
+	// -  LiftSwitchMax (1 port)
+	//
+	// Alternative is to eliminate the NavX board and use the
+	// REV More board.
+	//
+	// Alternative 2 is to use a MXP extension cable and stack
+	// the boards.
+	//
+	// TODO: Use PWM Y cables to consolidate the left and right 
+	// drive motors.
+	//
+
 //	public static final int[] DIO_DriveEncoderL = { 0, 1 };
 	public static final int[] DIO_DriveEncoderR = { 2, 3 };
 //	public static final int[] DIO_LiftEncoder   = { 4, 5 };
@@ -11,8 +26,7 @@ public class Ports {
 
 //	public static final int   DIO_LiftSwitchMax  = 10;
 //	public static final int   DIO_LiftSwitchMin  = 11;
-//	public static final int   DIO_LiftSwitchGrab = 12;
-//	public static final int   DIO_ClawSwitch     = 13;
+//	public static final int   DIO_ClawSwitch     = 12;
 	
 	
 //	public static final int[] PCM_ElevatorArms  = { 0, 1 };
@@ -33,6 +47,6 @@ public class Ports {
 	
 	
 	public static final int   USB_DriveStickL   = 0;
-	public static final int   USB_DriveStickR   = 1;
+//	public static final int   USB_DriveStickR   = 1;
 	
 }
